70b53e4ad39947f0c2294b623bcf6f3f754064af
Dolgov Dmytro <dimakherson74@gmail.com>
2025-04-08 01:16:06 +0200

    add to installed apps user and theatre

--- ticket_service/settings.py
+++ ticket_service/settings.py
@@ -37,9 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "theatre"
-    "user"
-
+    "theatre",
+    "user",
 ]
 
 MIDDLEWARE = [
