0843d6081499b9c44e9e88622b3ece2e25329ab4
Dolgov Dmytro <dimakherson74@gmail.com>
2025-04-08 01:23:06 +0200

    add to models.py classes Genre, Actor, Play, TheatreHall, Performance, Reservation and Ticket

--- theatre/models.py
+++ theatre/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from ticket_service import settings
 
-# Create your models here.
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Play(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    actors = models.ManyToManyField(Actor, related_name="plays", blank=True)
+    genres = models.ManyToManyField(Genre, related_name="plays", blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class TheatreHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.PositiveIntegerField()
+    seats_in_row = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Performance(models.Model):
+    play = models.ForeignKey(Play, on_delete=models.CASCADE, related_name="performances")
+    theatre_hall = models.ForeignKey(TheatreHall, on_delete=models.CASCADE, related_name="performances")
+    show_time = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.play.title} at {self.show_time}"
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="reservations")
+
+    def __str__(self):
+        return f"Reservation #{self.pk} by {self.user}"
+
+
+class Ticket(models.Model):
+    row = models.PositiveIntegerField()
+    seat = models.PositiveIntegerField()
+    performance = models.ForeignKey(Performance, on_delete=models.CASCADE, related_name="tickets")
+    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE, related_name="tickets")
+
+    class Meta:
+        unique_together = ("row", "seat", "performance")  # чтобы нельзя было забронировать 1 место дважды
+
+    def __str__(self):
+        return f"Row {self.row}, Seat {self.seat} for {self.performance}"
