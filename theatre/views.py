a1ea725c823ecc59cab8ad62f4791b645ec3c1c2
Dolgov Dmytro <dimakherson74@gmail.com>
2025-04-09 23:19:46 +0200

    add to theatre/views.py functions get_serializer_class, _params_to_ints and get_queryset

--- theatre/views.py
+++ theatre/views.py
@@ -3,7 +3,7 @@ from .models import Actor, Genre, Play, TheatreHall, Performance, Reservation, T
 from .serializers import (
     ActorSerializer, GenreSerializer, PlaySerializer,
     TheatreHallSerializer, PerformanceSerializer,
-    ReservationSerializer, TicketSerializer
+    ReservationSerializer, TicketSerializer, PlayListSerializer, PlayDetailSerializer
 )
 
 class ActorViewSet(viewsets.ModelViewSet):
@@ -17,6 +17,34 @@ class GenreViewSet(viewsets.ModelViewSet):
 class PlayViewSet(viewsets.ModelViewSet):
     queryset = Play.objects.all()
     serializer_class = PlaySerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PlayListSerializer
+
+        if self.action == "retrieve":
+            return PlayDetailSerializer
+
+        return PlaySerializer
+
+    @staticmethod
+    def _params_to_ints(query_string):
+        return [int(str_id) for str_id in query_string.split(",")]
+
+    def get_queryset(self):
+        queryset = Play.objects.all()
+        actors = self.request.query_params.get("actors")
+        genres = self.request.query_params.get("genres")
+        title = self.request.query_params.get("title")
+        if actors:
+            queryset = queryset.filter(actors=actors)
+        if genres:
+            genres = self._params_to_ints(genres)
+            queryset = queryset.filter(genres__id__in=genres)
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+        return queryset.distinct().prefetch_related("genres", "actors")
+
 
 class TheatreHallViewSet(viewsets.ModelViewSet):
     queryset = TheatreHall.objects.all()
