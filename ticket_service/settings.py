1451fee098cf2a1a6af1fe3d12d0f06eb6f0af36
Dolgov Dmytro <dimakherson74@gmail.com>
2025-04-11 18:34:28 +0200

    add to ticket_service/settings.py from dotenv import load_dotenv, env_path and SECRET_KEY = os.getenv(SECRET_KEY)

--- ticket_service/settings.py
+++ ticket_service/settings.py
@@ -10,18 +10,21 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
+from dotenv import load_dotenv
 from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+env_path = Path(__file__).resolve().parent.parent / ".env"
+load_dotenv(dotenv_path=env_path)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-3z74&=@!!agpyx@v^z35o1#1v47r)p_24w-2@irk9!g0jo*ogu"
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
